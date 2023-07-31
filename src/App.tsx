7d7a0df6b5166c23e9b27a6be73c64cc47b308bb
hjs0522 <91768822+hjs0522@users.noreply.github.com>
2023-08-01 03:03:43 +0900

    변환법및 설명 추가

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,9 @@ function App() {
   return (
     <div className="App">
       <header className='App-header'>옛 한글 조합기</header>
-      <HangulConverter></HangulConverter>
+      <div className='body'>
+        <HangulConverter></HangulConverter>
+      </div>
     </div>
   );
 }
--- src/HangulConverter.tsx
+++ src/HangulConverter.tsx
@@ -1,4 +1,4 @@
-import React, { useState,useRef,useEffect } from 'react';
+import React, { useState,useRef } from 'react';
 
 const HangulConverter: React.FC = () => {
     const [output, setOutput] = useState('');
@@ -14,83 +14,112 @@ const HangulConverter: React.FC = () => {
         'ㅁ': 'ㅿ',
         'ㅇ': 'ㆁ',
         'ㅎ': 'ㆆ',
-        'ㅋ': 'ᄼ',
-        'ᄼ': 'ᄽ',
-        'ㅌ': 'ᄾ',
-        'ᄾ': 'ᄿ',
-        'ㅊ': 'ᅎ',
-        'ᅎ': 'ᅏ',
-        'ㅍ': 'ᅐ',
-        'ᅐ': 'ᅑ',
-        'ㅠ': 'ᅔ',
-        'ㅜ': 'ᅕ'
-    };
-    useEffect(()=>{
-        setCursorPosition(textareaRef.current?.selectionStart ||0)
-    },[output])
-    const handleKeyUp = (event:React.KeyboardEvent) => {
-        setCursorPosition(textareaRef.current?.selectionStart ||0)
-        if (event.key === '.'){
+        'ㅏ': 'ㆍ',
+        'ㅂ': 'ㅸ',
+        'ㄹ': 'ㆅ',
+    }
+    
+    const handleKeyUp = (e:React.KeyboardEvent) => {
+        if (e.key === '.'){
             const newWord = convertHangul[lastWord];
             if (newWord){
-                setOutput(output.slice(0,cursorPosition-1)+newWord+output.slice(cursorPosition));
+                //.을 찍는 순간 handleInputChange에 의해 output의 길이가 1증가한다
+                setOutput(output.slice(0,cursorPosition-2)+newWord+output.slice(cursorPosition));
                 setLastWord(newWord);
             }else{
-                setOutput(output.slice(0,cursorPosition)+'.'+output.slice(cursorPosition))
+                setOutput(output.slice(0,cursorPosition-1)+'.'+output.slice(cursorPosition))
                 setLastWord('.')
             }
-
         }
-        else if(event.key === 'ㅏ' && lastWord ==='ㅏ'){
-            setOutput(output.slice(0,cursorPosition-1)+'ㆍ'+output.slice(cursorPosition))
-            setLastWord('ㆍ')
-        }
-        else if (event.shiftKey)
-        {   const newWord = convertHangul[event.key]
-            if (newWord)
-            {
-                if (lastWord === newWord && convertHangul[newWord]){
-                    setOutput(output.slice(0,cursorPosition-1)+convertHangul[newWord]+output.slice(cursorPosition));
-                    setLastWord(convertHangul[newWord]);
-                }
-                else{
-                    setOutput(output.slice(0,cursorPosition)+newWord+output.slice(cursorPosition));
-                    setLastWord(newWord);
-                }
-            }else{
-                setOutput(output.slice(0,cursorPosition)+event.key+output.slice(cursorPosition));
-                setLastWord(event.key);
-            }
-        }
-        else if(Arrow.includes(event.key)){
+        else if(Arrow.includes(e.key)){
             setCursorPosition(textareaRef.current?.selectionStart||0);
         }
-        else if(preventWord.includes(event.key)){
-            if (event.key !== 'Shift'){
+        else if(preventWord.includes(e.key)){
+            if (e.key !== 'Shift'){
                 setLastWord('');
             }
-            event.preventDefault();
-        }
-        else if(event.key==='Backspace'){
-            setOutput(output.slice(0,cursorPosition-1)+output.slice(cursorPosition+1))
-            setLastWord(output[cursorPosition-2])
-        }
-        else if(event.key ==='Space'){
-            setOutput(output.slice(0,cursorPosition)+' '+output.slice(cursorPosition))
-            setLastWord(' ')
-        }
-        else if(event.key ==='Enter'){
-            setOutput(output.slice(0,cursorPosition) + '\n' + output.slice(cursorPosition));
-            setLastWord('\n')
+            e.preventDefault();
         }
         else{
-            setOutput(output.slice(0,cursorPosition)+event.key+output.slice(cursorPosition));
-            setLastWord(event.key)
+            setLastWord(e.key)
         }
+        
     }
+    const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        const value = e.target.value;
+        const result: string[] = [];
+        let cho: number = -1;
+        let jung: number = -1;
+        let jong: number = -1;
+    
+        for (const ch of value) {
+          const code = ch.charCodeAt(0);
+    
+          if (0x1100 <= code && code <= 0x1112) { // 초성
+            if (cho !== -1 && jung !== -1) {
+              result.push(String.fromCharCode(0xAC00 + (cho * 588) + (jung * 28) + (jong !== -1 ? jong + 1 : 0)));
+              cho = jung = jong = -1;
+            }
+            cho = code - 0x1100;
+          } else if (0x1161 <= code && code <= 0x1175) { // 중성
+            if (cho !== -1 && jung !== -1) {
+              result.push(String.fromCharCode(0xAC00 + (cho * 588) + (jung * 28) + (jong !== -1 ? jong + 1 : 0)));
+              cho = jung = jong = -1;
+            }
+            jung = code - 0x1161;
+          } else if (0x11A7 < code && code <= 0x11C2) { // 종성
+            jong = code - 0x11A8;
+          } else { // 그 외 문자
+            if (cho !== -1 && jung !== -1) {
+              result.push(String.fromCharCode(0xAC00 + (cho * 588) + (jung * 28) + (jong !== -1 ? jong + 1 : 0)));
+              cho = jung = jong = -1;
+            }
+            result.push(ch);
+          }
+        }
+    
+        if (cho !== -1 && jung !== -1) {
+          result.push(String.fromCharCode(0xAC00 + (cho * 588) + (jung * 28) + (jong !== -1 ? jong + 1 : 0)));
+        }
+        setCursorPosition(textareaRef.current?.selectionStart ||0)
+        setOutput(result.join(''));
+      };
+    
+    
     return (
-        <div onKeyUp={handleKeyUp}>
-            <textarea ref={textareaRef} value={output}></textarea>
+        <div className={'container'} onKeyUp = {handleKeyUp}>
+            <textarea className={'text'} ref={textareaRef} onChange={handleInputChange} value={output}></textarea>
+            <div>
+                <p>
+                훈민정음의 28자모: 훈민정음의 초성과 종성에 쓰이는 자음 문자 17개와 중성에 쓰이는 모음 문자 11개를 통틀어 이르는 글자.
+                1443년에 창제된 우리나라 문자인 “훈민정음(=한글)”에 포함된, 초성과 종성에 쓰이는 자음 문자 17개와 중성에 쓰이는 모음 문자 11개를 통틀어 이르는 말이다.
+                </p>
+                <p>
+                『훈민정음』「예의」에는 처음 만들어진 문자인 ‘훈민정음’에 대한 음가와 운용 방법을 설명하고 있는데, 자음 ‘ ㄱ, ㅋ, ㆁ’, ‘ ㄷ, ㅌ, ㄴ’, ‘ ㅂ, ㅍ, ㅁ’, ‘ ㅈ, ㅊ, ㅅ’, ‘ㆆ, ㅎ, ㅇ’, ‘ ㄹ’, ‘ㅿ’의 17개와 모음 ‘ ㆍ, ㅡ, ㅣ, ㅗ, ㅏ, ㅜ, ㅓ, ㅛ, ㅑ, ㅠ, ㅕ’의 11개가 실려 있다.
+                </p>
+                <p>
+                    <strong>변환법</strong>
+                    <ul>
+                        <li>'ㅁ' + '.' : 'ㅿ'</li>
+                        <li>'ㅇ' + '.' : 'ㆁ'</li>
+                        <li>'ㅎ' + '.' : 'ㆆ'</li>
+                        <li>'ㅏ' + '.' : 'ㆍ'</li>
+                        <li>'ㄹ' + '.' : 'ㆅ'</li>
+                    </ul>
+                </p>
+                <p>
+                    <strong>참고문헌</strong>
+                    <ul>
+                        <li>『훈민정음 창제와 연구사』(강신항, 경진, 2010)</li>
+                        <li>『훈민정음신연구』(이근수, 보고사, 1995)</li>
+                        <li>『훈민정음연구』(강신항, 보고사, 1987)</li>
+                        <li>『개정판 국어사개설』(이기문, 탑출판사, 1972)</li>
+                        <li>｢중국 문자학과 『훈민정음』문자 이론｣(김주필, 『인문과학연구』48, 2005)</li>
+                        <li>｢한글의 과학성과 독착성｣(김주필, 『국제고려학회 논문집』창간호, 1999)</li>
+                    </ul>
+                </p>
+                <p>출처 : <a href='https://encykorea.aks.ac.kr/Article/E0074518'>한국민족문화 대백과 사전</a></p>
+            </div>
         </div>
     );
 }
